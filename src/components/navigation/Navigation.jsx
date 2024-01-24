20e9947ec3b5d1c0d76b14467df7ab10f0eee45f
karimaltohamy <karimbadr2003@gmail.com>
2024-01-24 16:59:52 +0200

    commit

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -14,24 +14,28 @@ const Navigation = () => {
           className={({ isActive }) => (isActive ? "link active" : "link")}
         >
           <CgProfile size={25} />
+          <span>Profile</span>
         </NavLink>
         <NavLink
           to={"/"}
           className={({ isActive }) => (isActive ? "link active" : "link")}
         >
           <FiHome size={25} />
+          <span>Home</span>
         </NavLink>
         <NavLink
           to={"/notification"}
           className={({ isActive }) => (isActive ? "link active" : "link")}
         >
           <RiNotification2Line size={25} />
+          <span>Notification</span>
         </NavLink>
         <NavLink
           to={"/record-reports"}
           className={({ isActive }) => (isActive ? "link active" : "link")}
         >
           <LuClipboardList size={25} />
+          <span>Reports</span>
         </NavLink>
       </div>
     </div>
